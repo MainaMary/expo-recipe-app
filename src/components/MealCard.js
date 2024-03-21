82b7fd74b34a10c90c12408ae155f57b65fde0e4
Mary <marieshix98@gmail.com>
2024-03-21 22:01:49 +0300

    display a list of recipes

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,11 +1,38 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native'
 import React from 'react'
-import { Image } from 'react-native'
-
-export default function MealCard({meals}) {
-  return (
-    <View>
-      <Text>MealCard</Text>
+import { heightPercentageToDP } from 'react-native-responsive-screen'
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
+    elevation: 3, // For Android shadow
+    shadowColor: '#000', // For iOS shadow
+    shadowOpacity: 0.25, // For iOS shadow
+    shadowRadius: 3, // For iOS shadow
+  },
+  image: {
+    width: '100%',
+    height: heightPercentageToDP(25),
+  },
+  text: {
+    padding: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+export default function MealCard({meal}) {
+ return (
+    <View style={styles.container}>
+      <Pressable style={styles.image}>
+        <Image 
+          source={{ uri: meal?.strMealThumb }}
+          style={styles.image}
+        />
+      </Pressable>
+      <Text style={styles.text}>
+        {meal?.strMeal.length > 25 ? `${meal?.strMeal.slice(0, 20, -3)}...` : meal?.strMeal}
+      </Text>
     </View>
-  )
+  );
 }
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MealCard from "./MealCard";
+import { useNavigation } from "@react-navigation/native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import Loader from "./Loader";
+import Animated, { FadeInDown } from "react-native-reanimated";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+});
+function Meals({ meals, categories }) {
+  const navigation = useNavigation();
 
-export default function Meals() {
   return (
-    <View>
-      <Text>Meals</Text>
-    </View>
-  )
+    <Animated.View
+      className="mx-4  space-y-4"
+      entering={FadeInDown.delay(200).duration(700).springify().damping(12)}
+    >
+      <Text
+        style={{
+          fontSize: hp(2),
+        }}
+        className="font-semibold text-neutral-600"
+      >
+        {meals.length} Recipes
+      </Text>
+
+      <Animated.View
+        entering={FadeInDown.delay(200).duration(700).springify().damping(12)}
+      >
+        {categories?.length == 0 || meals?.length == 0 ? (
+          <Loader size="large" className="mt-20" />
+        ) : (
+          <View style={styles.container}>
+            {meals.map((meal, index) => (
+              <View style={styles.row} key={index}>
+                <MealCard meal={meal} />
+                {/* Render another MealCard if it exists */}
+                {meals[index + 1] && <MealCard meal={meals[index + 1]} />}
+              </View>
+            ))}
+          </View>
+        )}
+      </Animated.View>
+    </Animated.View>
+  );
 }
+export default Meals;
