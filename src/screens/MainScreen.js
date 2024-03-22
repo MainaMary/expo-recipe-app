a0a1583281234bbe48898e7a5b389f584b2c0a7c
Mary <marieshix98@gmail.com>
2024-03-22 10:16:43 +0300

    code clean up

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,38 +1,47 @@
-import { View, Text, StatusBar, TouchableOpacity, ImageBackground } from 'react-native'
+import { View, Text, StatusBar, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native'
 import React, { useRef } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
-import {Animated} from 'react-native-reanimated'
-import {Lottie} from 'lottie-react-native'
 import MainImage from '../../assets/images/welcome.jpg'
-
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    backgroundColor: "#f0f0f0"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  welcome: { color: "white", fontWeight: "bold", fontSize: 20 },
+  explore: { color: "white", fontSize: 16 },
+  btnWrapper: { backgroundColor: "orange", padding: 10, borderRadius: 5, marginTop: 20 },
+  start: {
+    color: "white", textAlign: "center"
+  }
+})
 export default function MainScreen() {
   const navigation = useNavigation()
   const animation = useRef(null)
   return (
-   <ImageBackground 
-      source={MainImage} 
-      style={{
-        flex: 1,
-        resizeMode: "cover",
-        justifyContent: "center",
-        backgroundColor: "#f0f0f0"
-      }}
+    <ImageBackground
+      source={MainImage}
+
+      style={styles.main}
     >
-      <StatusBar style="light"/>
-      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text style={{color: "white", fontWeight: "bold", fontSize: 20}}>Welcome to BaoBox Food app</Text>
-        <Text style={{color: "white", fontSize:16}}>Explore delicious food</Text>
+      <StatusBar style="light" />
+      <View
+        style={styles.container}>
+        <Text
+          style={styles.welcome}>Welcome to BaoBox Food app</Text>
+        <Text style={styles.explore}>Explore delicious food</Text>
         <TouchableOpacity
-        onPress={() =>{navigation.navigate("Home")}}
-        style={{backgroundColor: "orange", padding: 10, borderRadius: 5, marginTop: 20}}>
-          <Text  
-          
-          className="px-3 py-2 text-base font-semibold" style={{
-            //  padding:"2px 3px",
-            // fontSize:"16px",
-            // fontWeight:"semibold",
-            color: "white", textAlign: "center"}}>Click to get started</Text>
+          onPress={() => { navigation.navigate("Home") }}
+          style={styles.btnWrapper}>
+          <Text
+            style={styles.start}
+          >Click to get started</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
