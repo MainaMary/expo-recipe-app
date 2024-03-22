bb7fa435817eca2ba4b218cd27ae80f6e2f71142
Mary <marieshix98@gmail.com>
2024-03-22 10:16:15 +0300

    use stylesheet from rn

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,51 +1,118 @@
-import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import Loader from './Loader';
+const styles = StyleSheet.create({
 
-function Category({categories, isCategoryActive, handleCategoryChange }) {
+  categoryContainer: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  selectedCategoryContainer: {
+    backgroundColor: 'orange',
+    borderRadius: 10,
+  },
+  imageContainer: {
+    width: hp(5),
+    height: hp(5),
+    borderRadius: hp(2.5),
+    overflow: 'hidden',
+    marginBottom: 5,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  categoryName: {
+    fontSize: hp(1.4),
+    color: 'black',
+    marginTop: 5,
+    marginBottom: 5
+  },
+  loader:{
+    marginTop: 20,
+    marginBottom:20,
+    textAlign:'center'
+  }
+});
+function Category({ categories, isCategoryActive, handleCategoryChange, isCategoryLoading }) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const handlePress = (index, category) => {
+    setSelectedIndex(index);
+    handleCategoryChange(category);
+
+  }
   return (
     <View>
       <ScrollView horizontal
         showsHorizontalScrollIndicator
-        className=""
+
         contentContainerStyle={{
-          paddingHorizontal: 15
+          paddingTop: 20,
+          paddingBottom: 20,
+          display: 'gap',
+          gap: 10,
+          marginTop: 5,
+          marginBottom: 5,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
         }}
       >
-        {categories?.map((category, index) => {
-          console.log({category})
-          return(
+        {
+          isCategoryLoading ? (
+          <Loader size="large" style={styles.loader} />
+        ) :
+          categories?.map((category, index) => {
+
+            return (
+             
+              
              <TouchableOpacity
           className="flex items-center space-y-1"
           key={index}
-           onPress ={() => handleCategoryChange(category?.strCategory)}>
+          onPress={() => handlePress(index, category?.strCategory)}
+          
+           >
           <View 
           
-           style={{backgroundColor : category.strCategory == isCategoryActive ? 'orange':'',
-           borderRadius: 10
+           style={{
+            backgroundColor : category.strCategory == isCategoryActive && selectedIndex == index ? 'orange':'white',
+           borderRadius: 10,
+           padding: 4,
+           width: hp(8),
+           height: hp(8),
            }}
-          className ={ category.strCategory == isCategoryActive ? "bg-orange rounded-xl" :"rounded-xl bg white"}>
+          >
             <Image
               source={{
                 uri: category.strCategoryThumb
               }}
-              style={{
-                width: hp(5),
-                height: hp(5)
-              }}
+              style={styles.image}
             />
+            <Text 
+         style={styles.categoryName}
+         >{category.strCategory}</Text>
           </View>
-         <Text 
-         style={{
-          fontSize: hp(1.4)
-         }}
-         className="text-neutral-900">{category.strCategory}</Text>
+         
         </TouchableOpacity>
+             
+            
+            )
+          }
           )
         }
-       )}
       </ScrollView>
-      <Text>Categories</Text>
+       <Text style={{
+              fontSize: hp(1.8)
+            }}
+              className="font-semibold"
+            >
+            Category: <Text style={{
+                color: 'orange',
+                fontWeight: 'semibold'
+              }}>{isCategoryActive}</Text>
+            </Text>
     </View>
   )
 }
