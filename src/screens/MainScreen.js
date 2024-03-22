4b4ec17147aee75e513dc3f9e321d3bcc06458da
Mary <marieshix98@gmail.com>
2024-03-22 11:32:30 +0300

    code clean up

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,49 +1,57 @@
-import { View, Text, StatusBar, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native'
-import React, { useRef } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import MainImage from '../../assets/images/welcome.jpg'
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
+import React, { useRef } from "react";
+import { useNavigation } from "@react-navigation/native";
+import MainImage from "../../assets/images/welcome.jpg";
 const styles = StyleSheet.create({
   main: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
-    backgroundColor: "#f0f0f0"
+    backgroundColor: "#f0f0f0",
   },
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   welcome: { color: "white", fontWeight: "bold", fontSize: 20 },
   explore: { color: "white", fontSize: 16 },
-  btnWrapper: { backgroundColor: "orange", padding: 10, borderRadius: 5, marginTop: 20 },
+  btnWrapper: {
+    backgroundColor: "orange",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
   start: {
-    color: "white", textAlign: "center"
-  }
-})
+    color: "white",
+    textAlign: "center",
+  },
+});
 export default function MainScreen() {
-  const navigation = useNavigation()
-  const animation = useRef(null)
+  const navigation = useNavigation();
+  const animation = useRef(null);
   return (
-    <ImageBackground
-      source={MainImage}
-
-      style={styles.main}
-    >
+    <ImageBackground source={MainImage} style={styles.main}>
       <StatusBar style="light" />
-      <View
-        style={styles.container}>
-        <Text
-          style={styles.welcome}>Welcome to BaoBox Food app</Text>
-        <Text style={styles.explore}>Explore delicious food</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to BaoBox Meal app</Text>
+        <Text style={styles.explore}>Explore delicious foods</Text>
         <TouchableOpacity
-          onPress={() => { navigation.navigate("Home") }}
-          style={styles.btnWrapper}>
-          <Text
-            style={styles.start}
-          >Click to get started</Text>
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+          style={styles.btnWrapper}
+        >
+          <Text style={styles.start}>Click to get started</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
-  )
+  );
 }
